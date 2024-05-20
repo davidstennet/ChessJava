03f10d4289dfafe78fd7cf08a944395803509a1f
davidstennet <david@stennet.com>
2024-05-19 20:54:51 -0700

    Added Queen implementation, just used the rook and bishop code

--- Battle.java
+++ Battle.java
@@ -336,7 +336,64 @@ public class Battle {
     }
     
     private void clickedOnQueen(String colRow, JLabelBox square, HashMap<String, JLabelBox> mapOfBoxes) {
+        Queen clickedQueen = (Queen)clickedPiece; // Downcasts the pawn
+        // boolean for whether the spot they are moving to is valid
+        boolean canMove = clickedQueen.canMove(square.getColumn(), square.getRow(), this.white.getPieceLocation(), this.black.getPieceLocation()); 
 
+        if (clickedQueen.isWhite() && canMove && this.isWhiteTurn) { // Moving piece after being clicked
+            // Sets image of original piece to nothin
+            String originalLocation = Integer.toString(clickedPiece.getColumn()) + Integer.toString(clickedPiece.getRow());
+            JLabelBox originalBox = mapOfBoxes.get(originalLocation);
+            originalBox.setIcon(null);
+            white.getPieceLocation().remove(originalLocation);
+
+            // Sets new locations
+            clickedPiece.setColumn(square.getColumn());
+            clickedPiece.setRow(square.getRow());
+            String newLocation = Integer.toString(clickedPiece.getColumn()) + Integer.toString(clickedPiece.getRow());
+            white.getPieceLocation().put(newLocation, clickedPiece);
+            if (this.black.getPieceLocation().containsKey(newLocation)) { // Gets rid of piece in opponent hashmap if this is an attacking move
+                this.black.getPieceLocation().remove(newLocation);
+            }
+
+
+            ImageIcon icon = new ImageIcon(clickedPiece.getImgURL());
+            square.setIcon(icon);
+
+            firstClick = true;
+            clickedPiece = null;
+            this.isWhiteTurn = false;
+        }
+        else if (!clickedQueen.isWhite() && canMove && !this.isWhiteTurn) {
+            // Sets image of original piece to nothin
+            String originalLocation = Integer.toString(clickedPiece.getColumn()) + Integer.toString(clickedPiece.getRow());
+            JLabelBox originalBox = mapOfBoxes.get(originalLocation);
+            originalBox.setIcon(null);        clickedPiece.getName();
+
+            black.getPieceLocation().remove(originalLocation);
+
+            // Sets new locations
+            clickedPiece.setColumn(square.getColumn());
+            clickedPiece.setRow(square.getRow());
+            String newLocation = Integer.toString(clickedPiece.getColumn()) + Integer.toString(clickedPiece.getRow());
+            black.getPieceLocation().put(newLocation, clickedPiece);
+            if (this.white.getPieceLocation().containsKey(newLocation)) { // Gets rid of piece in opponent hashmap if this is an attacking move
+                this.white.getPieceLocation().remove(newLocation);
+            }
+
+
+            ImageIcon icon = new ImageIcon(clickedPiece.getImgURL());
+            square.setIcon(icon);
+
+
+            firstClick = true;
+            clickedPiece = null;
+            this.isWhiteTurn = true;
+        }
+        else {
+            clickedPiece = null;
+            firstClick = true;
+        }
     }
     
     private void clickedOnKing(String colRow, JLabelBox square, HashMap<String, JLabelBox> mapOfBoxes) {
--- Queen.java
+++ Queen.java
@@ -7,6 +7,195 @@ public class Queen extends Piece {
 	}
 
 	public boolean canMove(int targetColumn, int targetRow, HashMap<String, Piece> whitePieces, HashMap<String, Piece> blackPieces) {
+		String colRow = Integer.toString(targetColumn) + Integer.toString(targetRow);
+		int rowDif = targetRow - this.row;
+		int colDif = targetColumn - this.column;
+
+		if (this.isWhite) {
+			if (whitePieces.containsKey(colRow)) {
+				return false;
+			}
+			else if (Math.abs(rowDif) == Math.abs(colDif)) {
+				if (rowDif > 0 && colDif > 0) {
+					int j = 1;
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(this.column + j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif < 0 && colDif > 0) {
+					int j = 1;
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(this.column + j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif > 0 && colDif < 0) {
+					int j = 1;
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(this.column - j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif < 0 && colDif < 0) {
+					int j = 1;
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(this.column - j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				return true;
+			}
+			else if (targetRow == this.row) { // Checks that the target is in the same row
+				if (targetColumn > this.column) {
+					// This for loop will go through all of the pieces that are in front of where the user clicked to see if it is allowed to move there
+					for (int i = this.column + 1; i < targetColumn; i++) {
+						String testColRow = Integer.toString(i) + Integer.toString(targetRow);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+				else if (targetColumn < this.column) { 
+										// This for loop will go through all of the pieces that are in front of where the user clicked to see if it is allowed to move there
+
+					for (int i = this.column - 1; i > targetColumn; i--) {
+						String testColRow = Integer.toString(i) + Integer.toString(targetRow);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			else if (targetColumn == this.column) {
+				if (targetRow > this.row) {
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(targetColumn) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+				else if (targetRow < this.row) {
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(targetColumn) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			
+		}
+		else {
+			if (blackPieces.containsKey(colRow)) {
+				return false;
+			}
+			else if (Math.abs(rowDif) == Math.abs(colDif)) {
+				if (rowDif > 0 && colDif > 0) {
+					int j = 1;
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(this.column + j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif < 0 && colDif > 0) {
+					int j = 1;
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(this.column + j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif > 0 && colDif < 0) {
+					int j = 1;
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(this.column - j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				else if (rowDif < 0 && colDif < 0) {
+					int j = 1;
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(this.column - j) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+						j++;
+					}
+				}
+				return true;
+			}
+			else if (targetRow == this.row) { // Checks that the target is in the same row
+				if (targetColumn > this.column) {
+					// This for loop will go through all of the pieces that are in front of where the user clicked to see if it is allowed to move there
+					for (int i = this.column + 1; i < targetColumn; i++) {
+						String testColRow = Integer.toString(i) + Integer.toString(targetRow);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+				else if (targetColumn < this.column) { 
+										// This for loop will go through all of the pieces that are in front of where the user clicked to see if it is allowed to move there
+
+					for (int i = this.column - 1; i > targetColumn; i--) {
+						String testColRow = Integer.toString(i) + Integer.toString(targetRow);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			else if (targetColumn == this.column) {
+				if (targetRow > this.row) {
+					for (int i = this.row + 1; i < targetRow; i++) {
+						String testColRow = Integer.toString(targetColumn) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+				else if (targetRow < this.row) {
+					for (int i = this.row - 1; i > targetRow; i--) {
+						String testColRow = Integer.toString(targetColumn) + Integer.toString(i);
+						if (whitePieces.containsKey(testColRow) || blackPieces.containsKey(testColRow)) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			
+		}
+
 		return false;
 	}
 
